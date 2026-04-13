89b9f391e8a5e5d211d44370f67de2ce8cce8924
Ricardo <RickyPlayMail@gmail.com>
2026-04-13 00:33:38 -0400

    z

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -34,7 +34,7 @@ const LOCATIONS = [
 
 function getPromoProducts(products = []) {
   const availableProducts = products.filter((product) => product.available);
-  const promotions = availableProducts.filter((product) => product.category === 'Postres');
+  const promotions = availableProducts.filter((product) => product.category === 'Promociones');
 
   if (promotions.length > 0) {
     return promotions.slice(0, 6);
